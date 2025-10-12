9c5c80f558593ad073a2d491dff5c89a66f7b4c9
Anthony COUDENE <anthony.coudene@technidata-web.com>
2025-10-12 14:21:42 +0200

    Add BlazorApp, ApiApp, reorganize, add architecture tests and so on.

---
+++ tests/Company.Feature.Application.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace Company.Feature.Application.Tests
+{
+  public class UnitTest1
+  {
+    [Fact]
+    public void Test1()
+    {
+
+    }
+  }
+}
---
+++ tests/Company.Features.Architecture.Tests/ArchitectureTests.cs
@@ -0,0 +1,18 @@
+﻿using NetArchTest.Rules;
+
+namespace Company.Features.Architecture.Tests;
+
+public class ArchitectureTests
+{
+  [Fact]
+  public void Domain_Should_Not_Depend_On_Application()
+  {
+    var result = Types
+        .InAssembly(typeof(Company.Feature.Domain.Entities.EntityName).Assembly)
+        .ShouldNot()
+        .HaveDependencyOn("Company.Feature.Application")
+        .GetResult();
+
+    Assert.True(result.IsSuccessful);
+  }
+}
