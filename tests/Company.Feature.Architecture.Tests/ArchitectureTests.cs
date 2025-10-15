3a27be85cb2127b55c45186b872b45dc503acda9
Anthony COUDENE <anthony.coudene@technidata-web.com>
2025-10-15 18:38:09 +0200

    Architecture tests

--- tests/Company.Feature.Architecture.Tests/ArchitectureTests.cs
+++ tests/Company.Feature.Architecture.Tests/ArchitectureTests.cs
@@ -3,25 +3,55 @@
 namespace Company.Feature.Architecture.Tests;
 
 public class ArchitectureTests
-{  
+{
+  private const string assemblyDomain = "Company.Feature.Domain";
+  private const string assemblyUseCases = "Company.Feature.UseCases";
+
+  private static List<string> GetInvalidDependencies(string assemblyName, string[] allowedPrefixes)
+  {
+    /// Arrange
+    var assembly = Assembly.Load(assemblyName);
+
+    /// Act
+    var referencedAssemblies = assembly.GetReferencedAssemblies();
+    var invalidRefs = referencedAssemblies
+      .Where(a => !allowedPrefixes.Any(prefix => a.Name is not null && a.Name.StartsWith(prefix)))
+      .Select(a => (a.Name is not null) ? a.Name : string.Empty)
+      .ToList();
+    return invalidRefs;
+  }
+
   [Fact]
   public void Domain_Should_Not_Depend_On_Any_Dependencies_Except_DotNet_Framework()
   {
     /// Arrange
-    string[] AllowedPrefixes = [ "System", "Microsoft", "netstandard" ];    
-    var assembly = Assembly.Load("Company.Feature.Domain");
-    
-    /// Act
-    var referencedAssemblies = assembly.GetReferencedAssemblies();
-    var invalidRefs = referencedAssemblies
-        .Where(a => !AllowedPrefixes.Any(prefix => string.IsNullOrWhiteSpace(a.Name) || a.Name.StartsWith(prefix)))
-        .Select(a => a.Name)
-        .ToList();
+    string selectedAssemblyName = assemblyDomain;
+    string[] allowedPrefixes = ["System", "Microsoft", "netstandard"];
+
+    /// Act    
+    List<string> invalidRefs = GetInvalidDependencies(selectedAssemblyName, allowedPrefixes);
 
     /// Assert
     Assert.True(
         invalidRefs.Count == 0,
-        $"{assembly.GetName().Name} has not allowed dependencies: {string.Join(", ", invalidRefs)}"
+        $"{selectedAssemblyName} has not allowed dependencies: {string.Join(", ", invalidRefs)}"
+    );
+  }
+
+  [Fact]
+  public void UseCases_Should_Not_Depend_On_Any_Dependencies_Except_DotNet_Framework_And_Domain()
+  {
+    /// Arrange
+    string selectedAssemblyName = assemblyUseCases;
+    string[] allowedPrefixes = ["System", "Microsoft", "netstandard", assemblyDomain];
+
+    /// Act    
+    List<string> invalidRefs = GetInvalidDependencies(selectedAssemblyName, allowedPrefixes);
+
+    /// Assert
+    Assert.True(
+        invalidRefs.Count == 0,
+        $"{selectedAssemblyName} has not allowed dependencies: {string.Join(", ", invalidRefs)}"
     );
   }
 }
